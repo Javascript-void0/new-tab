be681e2060d74b8519b2c441a778740110dfbd04
Java <indead1124@gmail.com>
2023-03-23 21:35:31 -0400

    cleanup

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Web Browser Startup Page
+<h1>firefox new tab page</h1>
 
-![screenshot](assets/screenshot.png)
+![screenshot](assets/screenshot.jpg)
+
+## Requirements (Firefox)
+
+[Firefox Browser Developer Edition](https://www.mozilla.org/en-US/firefox/developer/) to install unsigned extensions
+
+## Fonts Used
+
+Envy Code R
+<br>[Space Grotesk](https://github.com/floriankarsten/space-grotesk)
--- script.js
+++ script.js
@@ -1,162 +1,38 @@
-// STATS
+const date = document.getElementById('date');
+date.textContent = new Date().toLocaleDateString();
 
-setInterval(setTime, 1000)
+const time = document.getElementById('time');
+time.textContent = new Date().toLocaleTimeString();;
+setInterval(()=>{
+    time.textContent = new Date().toLocaleTimeString();;
+}, 1000);
 
-function setTime() {
-    now = new Date()
-    totalMinutes = (now.getHours() * 60) + (now.getMinutes())
-    percent = totalMinutes / 1440
-    barCount = 10
-    filledBars = Math.floor(barCount * percent)
-    emptyBars = generateBars(barCount - filledBars)
-    filledBars = generateBars(filledBars)
+const face = document.getElementById('face');
+const ears = document.getElementById('ears');
+const ex = document.getElementById('ex');
+const qu = document.getElementById('qu');
 
-    document.getElementById('time').innerHTML = `
-        <br>time: ${now.toLocaleTimeString()}
-        <br>date: ${now.toLocaleDateString('en-US')}
-        <br>%day: <span class="bar-fill">${filledBars}</span><span class="bar-empty">${emptyBars}</span> (${Math.round(percent * 100)}%)
-    `
-}
+let i = 0;
 
-function generateBars(num) {
-    bars = ''
-    for (i = 0; i < num; i++) {
-        bars += '/'
+setInterval(()=>{
+    
+    if (i % 10 == 0) {
+        qu.style.visibility = '';
+    } else if (i % 5 == 0) {
+        ex.style.visibility = '';
+    } else {
+        ex.style.visibility = 'hidden';
+        qu.style.visibility = 'hidden';
     }
-    return bars
-}
-
-// ANIMATION
-
-setInterval(animate, 200)
-
-animation = [
-    '          (\\/)      ',
-    'ading...  (._.)   Lo',
-    '          / > >     '
-    ]
-
-function animate() {
-        
-    finalFrame = ''
-    aniBunny = document.getElementById('ascii-bunny')
-    for (i = 0; i < 3; i++) {
-        finalFrame += '<br>' + animation[i].replaceAll(' ', '&nbsp') + '\n'
-        last = animation[i].slice(-1)
-        rest = animation[i].slice(0, animation[i].length - 1)
-        animation[i] = last + rest
+    
+    if (i % 2 == 0) {
+        face.innerHTML = '._.&nbsp';
+        ears.innerHTML = '&nbsp&nbsp(\\/)';
+    } else {
+        face.innerHTML = '&nbsp._.';
+        ears.innerHTML = '(\\/)&nbsp&nbsp';
     }
-    aniBunny.innerHTML = finalFrame
-}
+    
+    i++;
 
-// ANIMATION 2
-
-setInterval(animate2, 180)
-
-frameNum = 0
-animation2 = [
-    [
-        "         ",
-        " _      __",
-        "|(     /_(",
-        "`.\\V)/.' ",
-        "  `\"-'   ",
-        "         ",
-        "         "
-    ],
-    [
-        "         ",
-        "         ",
-        "         ",
-        "   V)_   ",
-        "  /\"- \\  ",
-        "  |( |(  ",
-        "  \\( \\(  "
-    ],
-    [
-        "         ",
-        "         ",
-        "         ",
-        "  .V)-.  ",
-        " / \"-\\.\\ ",
-        " \\(   )/ ",
-        "         "
-    ],
-    [
-        "   _   __",
-        "  |(  /_(",
-        "  \\(  |\\(",
-        "   V)-`/ ",
-        "   \"-'   ",
-        "         ",
-        "         "
-    ],
-    [
-        "         ",
-        " _      __",
-        "|(     /_(",
-        "`.\\V)/.' ",
-        "  `\"-'   ",
-        "         ",
-        "         "
-    ],
-    [
-        "         ",
-        "         ",
-        "         ",
-        "   V)_   ",
-        "  /\"- \\  ",
-        "  |( |(  ",
-        "  \\( \\(  "
-    ],
-    [
-        "         ",
-        "         ",
-        "         ",
-        "  .V)-.  ",
-        " /  -\\.\\ ",
-        " \\(   )/ ",
-        "         "
-    ],
-    [
-        "   _   __",
-        "  |(  /_(",
-        "  \\(  |\\(",
-        "   V)-`/ ",
-        "   \"-'   ",
-        "         ",
-        "         "
-    ]
-]
-
-function animate2() {
-    frame = animation2[frameNum]
-    for (i = 0; i < frame.length; i++) {
-        frame[i] = frame[i].replaceAll(' ', '&nbsp;')
-    }
-    ele = document.getElementById('ascii-bat')
-    ele.innerHTML = frame.join("<br>")
-    frameNum++
-    if (frameNum > 7) {
-        frameNum = 0
-    }
-}
-
-// RESIZE
-
-const charWidth = 8.595
-
-setInterval(function() {
-    rightSideWidth = document.getElementById('content').offsetWidth - document.getElementById('content-left').offsetWidth
-    charFit = Math.floor(rightSideWidth / charWidth) - 3
-    titles = document.getElementsByClassName('link-titles')
-    for (title of titles) {
-        while (title.textContent.length < charFit) {
-            title.textContent = title.textContent + '-'
-        }
-        while (title.textContent.length > charFit) {
-            str = title.textContent
-            title.textContent = str.substring(0, str.length - 1)
-        }
-    }
-}, 10);
+}, 2236);
